99535d554201f94ec03b1a299a0bd520dac2c15d
Shelby Doolittle <shelbyd@google.com>
2016-02-02 18:25:07 -0500

    extract a class to represent a symbol

--- position_mutator.py
+++ position_mutator.py
@@ -7,7 +7,7 @@ class CommonEqualityMixin(object):
         return not self.__eq__(other)
 
     def __hash__(self):
-        return hash(self.__dict__)
+        return hash(frozenset(self.__dict__.iteritems()))
 
 class Position:
     def __init__(self, symbol, quantity):
@@ -23,35 +23,36 @@ class PositionMutator:
 
     def optimal_trades(self):
         buys = {}
-        for symbol in self.weights:
-            buys[symbol] = 0
+        for symbol in self.symbols():
+            buys[symbol.name] = 0
 
         remaining_cash = self.cash
         while True:
             symbol = self.lowest_equity(buys)
 
-            if remaining_cash <= self.prices[symbol]:
+            if remaining_cash <= symbol.price:
                 break
 
-            buys[symbol] += 1
-            remaining_cash -= self.prices[symbol]
+            buys[symbol.name] += 1
+            remaining_cash -= symbol.price
 
         return [Buy(buys[symbol], symbol) for symbol in buys if buys[symbol] > 0]
 
     def lowest_equity(self, buys):
-        return min(self.weights.keys(), key=lambda symbol: self.weighted_equity(symbol, buys))
-
-    def weighted_equity(self, symbol, buys):
-        quantity = self.position_for_symbol(symbol).quantity + buys[symbol]
-        price = self.prices[symbol]
-        weight = self.weights[symbol]
-
-        return price * quantity / weight
-
+        return min(self.symbols(buys), key=Symbol.weighted_equity)
 
     def position_for_symbol(self, symbol):
         possibilities = (p for p in self.positions if p.symbol == symbol)
         return next(possibilities, Position(symbol, 0.0))
+
+    def symbols(self, buys={}):
+        return [
+            Symbol(name,
+                   self.position_for_symbol(name).quantity + buys.get(name, 0),
+                   self.prices[name],
+                   self.weights[name])
+            for name in self.weights.keys()
+        ]
 
 class Buy(CommonEqualityMixin):
     def __init__(self, amount, symbol):
@@ -63,3 +64,13 @@ class Buy(CommonEqualityMixin):
 
     def execute_trade(self, trader):
         print "Buy %d of %s" % (self.amount, self.symbol)
+
+class Symbol(CommonEqualityMixin):
+    def __init__(self, name, quantity, price, weight):
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+        self.weight = weight
+
+    def weighted_equity(self):
+        return self.price * self.quantity / self.weight
