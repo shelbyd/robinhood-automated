d8a0535c49401a2a3b30e3acea06b17edccf319c
Shelby Doolittle <shelby@shelbyd.com>
2016-06-20 09:21:02 -0700

    refactor buy

--- buy.py
+++ buy.py
@@ -3,14 +3,15 @@ from main import Main
 
 import sys
 
-cash = None
-if len(sys.argv) > 1:
-    cash = float(sys.argv[1])
-
 class MainPurchaser(Main):
     def get_mutator(self):
+        if len(sys.argv) > 1:
+            cash = float(sys.argv[1])
+        else:
+            cash = self.cash()
+
         return Purchaser(
-                self.cash() if cash is None else cash,
+                cash,
                 self.positions(),
                 self.weights(),
                 self.prices())
