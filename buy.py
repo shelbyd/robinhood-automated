f46a8e392d8fdeaa2693389d3e0fa0b5d9e71ca7
Shelby Doolittle <shelby@shelbyd.com>
2016-06-20 09:13:21 -0700

    accept optional cash amount argument

--- buy.py
+++ buy.py
@@ -1,10 +1,16 @@
 from purchaser import Purchaser
 from main import Main
 
+import sys
+
+cash = None
+if len(sys.argv) > 1:
+    cash = float(sys.argv[1])
+
 class MainPurchaser(Main):
     def get_mutator(self):
         return Purchaser(
-                self.cash(),
+                self.cash() if cash is None else cash,
                 self.positions(),
                 self.weights(),
                 self.prices())
